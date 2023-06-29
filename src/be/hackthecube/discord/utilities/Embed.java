4773a201769418106e9958d0e94a36931928c696
Shykuzo <Stroobants.Lucas@proton.me>
2023-06-29 04:36:08 +0200

    [~] Embed | Added Methods & Constructor

--- src/be/hackthecube/discord/utilities/Embed.java
+++ src/be/hackthecube/discord/utilities/Embed.java
@@ -1,5 +1,52 @@
 package be.hackthecube.discord.utilities;
 
+import java.awt.Color;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
 public class Embed {
 
+	private EmbedBuilder embedBuilder;
+	
+		// -------------------- \\
+	
+	public Embed() {
+		embedBuilder = new EmbedBuilder()
+				.setColor(new Color(43, 45, 49));
+	}
+	
+		// -------------------- \\
+	
+	public Embed setTitle(String title) {
+		embedBuilder.setTitle(title);
+		return this;
+	}
+	
+	public Embed setDescription(String description) {
+		embedBuilder.setDescription(description);
+		return this;
+	}
+	
+	public Embed addField(String title, String description, boolean inline) {
+		embedBuilder.addField(title, description, inline);
+		return this;
+	}
+	
+	public Embed setThumbnail(String thumbnailURL) {
+		embedBuilder.setThumbnail(thumbnailURL);
+		return this;
+	}
+	
+	public Embed setFooter(String footer) {
+		embedBuilder.setFooter(footer);
+		return this;
+	}
+	
+		// -------------------- \\
+	
+	public MessageEmbed build() {
+		return embedBuilder.build();
+	}
+	
 }
