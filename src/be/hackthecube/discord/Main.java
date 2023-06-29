6bd6a26fb514057af590a96a3add8b07788087e7
Shykuzo <Stroobants.Lucas@proton.me>
2023-06-29 06:39:27 +0200

    [~] Main | Added Methods & Getter

--- src/be/hackthecube/discord/Main.java
+++ src/be/hackthecube/discord/Main.java
@@ -1,5 +1,51 @@
 package be.hackthecube.discord;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
 
+import be.hackthecube.discord.utilities.ConfigManager;
+import be.hackthecube.discord.utilities.annotation.Annotation;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Main extends JavaPlugin {
+
+	private static Main instance;
+	private JDA discordAPI;
+	
+	private ConfigManager configManager;
+	
+		// -------------------- \\
+	
+	public void onEnable() {
+		instance = this;
+		configManager = new ConfigManager();
+		
+			// ---------- \\
+		
+		saveDefaultConfig();
+		Annotation.loadClass(getConfig(), getConfigManager());
+		
+			// ---------- \\
+		
+		discordAPI = JDABuilder.createDefault(getConfigManager().getToken())
+				.setStatus(getConfigManager().getOnlineStatus())
+				.setActivity(getConfigManager().getDiscordActivity())
+				.addEventListeners()
+				.build();
+	}
+	
+		// -------------------- \\
+	
+	public static Main getInstance() {
+		return instance;
+	}
+	
+	public JDA getDiscordAPI() {
+		return discordAPI;
+	}
+	
+	public ConfigManager getConfigManager() {
+		return configManager;
+	}
+	
 }
